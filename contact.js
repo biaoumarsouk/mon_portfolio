d42c3f16f8f3fe103a677ab48c0429d60b452110
biaoumarsouk <biaoumarsouk@gmail.com>
2026-03-22 20:01:22 +0100

    Mise à jour du texte

--- contact.js
+++ contact.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
           },
           To: [
             {
-              Email: "ton-email-de-reception@gmail.com", // Où tu reçois le message
+              Email: "biaoumarsouk@gmail.com", // Où tu reçois le message
               Name: "Marsouk"
             }
           ],
